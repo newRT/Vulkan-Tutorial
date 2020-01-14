efe08af934681a47c5c6ce9f35d126d439dc3271
newRT <cgn00b2020@gmail.com>
2020-01-14 23:40:28 +0800

    VkInstance

--- Vulkan-Tutorial/main.cpp
+++ Vulkan-Tutorial/main.cpp
@@ -1,35 +1,118 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <stdexcept>
+#include <functional>
+#include <cstdlib>
 
-int main() {
-	glfwInit();
+// global const
+const int		WIDTH		= 800;
+const int		HEIGHT		= 600;
 
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-	std::cout << extensionCount << " extensions supported" << std::endl;
-
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	auto test = matrix * vec;
-
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
+class HelloTriangleApplication
+{
+public:
+	void Run()
+	{
+		_initWindow();
+		_initVulkan();
+		_mainLoop();
+		_cleanup();
 	}
 
-	glfwDestroyWindow(window);
+private:
+	GLFWwindow* _window;
 
-	glfwTerminate();
+	// vulkan
+	VkInstance _instance;
+private:
+	void _initWindow()
+	{
+		glfwInit();
 
-	return 0;
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);	// no openGL api
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);		// no resize
+
+		_window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
+	void _createInstance()
+	{
+		VkApplicationInfo appInfo = {};
+		appInfo.sType				= VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName	= "Hello Triangle";
+		appInfo.applicationVersion	= VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName			= "No Engine";
+		appInfo.engineVersion		= VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion			= VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo = {};
+		createInfo.sType			= VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+		std::cout << "available extension: " << std::endl;
+
+		for (const auto& extension : extensions)
+		{
+			std::cout << "\t\t" << extension.extensionName << std::endl;
+		}
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount	= glfwExtensionCount;
+		createInfo.ppEnabledLayerNames		= glfwExtensions;
+		createInfo.enabledLayerCount		= 0;
+
+		if (vkCreateInstance(&createInfo, nullptr, &_instance) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create instance!");
+		}
+	}
+	void _initVulkan()
+	{
+		_createInstance();
+	}
+
+	void _mainLoop()
+	{
+		while (!glfwWindowShouldClose(_window))
+		{
+
+		}
+	}
+
+	void _cleanup()
+	{
+		vkDestroyInstance(_instance, nullptr);
+
+		glfwDestroyWindow(_window);
+
+		glfwTerminate();
+	}
+};
+
+int main()
+{
+	HelloTriangleApplication app;
+
+	try
+	{
+		app.Run();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
