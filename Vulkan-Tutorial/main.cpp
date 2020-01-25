ba58b9d3de8ab00549b2d8175c5dbd1b7dd4de17
newRT <cgn00b2020@gmail.com>
2020-01-25 23:08:39 +0800

    create render pass

--- Vulkan-Tutorial/main.cpp
+++ Vulkan-Tutorial/main.cpp
@@ -134,6 +134,9 @@ private:
 
 	// pipeline layout
 	VkPipelineLayout					_pipelineLayout;
+
+	// render pass
+	VkRenderPass						_renderPass;
 private:
 
 	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
@@ -261,9 +264,45 @@ private:
 		_createLogicDevice();
 		_createSwapchain();
 		_createImageViews();
+		_createRenderPass();
 		_createGraphicsPipeline();
 	}
 	
+	void _createRenderPass()
+	{
+		VkAttachmentDescription colorAttachment = {};
+		colorAttachment.format = _swapChainImageFormat;
+		colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+		colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+		colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+		colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+		colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+		// SUBPASS
+		// attachment references
+		VkAttachmentReference colorAttachmentRef = {};
+		colorAttachmentRef.attachment = 0;
+		colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+		VkSubpassDescription subpass = {};
+		subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+		subpass.colorAttachmentCount = 1;
+		subpass.pColorAttachments = &colorAttachmentRef;
+
+		VkRenderPassCreateInfo renderPassInfo = {};
+		renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+		renderPassInfo.attachmentCount = 1;
+		renderPassInfo.pAttachments = &colorAttachment;
+		renderPassInfo.subpassCount = 1;
+		renderPassInfo.pSubpasses = &subpass;
+		if (vkCreateRenderPass(_device, &renderPassInfo, nullptr, &_renderPass) != VK_SUCCESS)
+		{
+			throw std::runtime_error("Failed to create render pass!");
+		}
+	}
+
 	void _createGraphicsPipeline()
 	{
 		auto vertShaderCode = readFile("shaders/vert.spv");
@@ -786,6 +825,8 @@ private:
 	void _cleanup()
 	{
 		vkDestroyPipelineLayout(_device, _pipelineLayout, nullptr);
+
+		vkDestroyRenderPass(_device, _renderPass, nullptr);
 
 		for (auto imageView : _swapChainImageViews)
 		{
